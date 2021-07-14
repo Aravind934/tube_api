2b6f489b771406c28ef4209f43eae7f16ed50b5c
root <aravindk6066@gmail.com>
2021-07-14 14:50:23 +0530

    api maked

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  uri: "https://www.youtube.com/feeds/videos.xml?channel_id=UCnT-RyIR6mwWlDOBIyEZY8A",
+  port: 8000,
+};
---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const { port } = require("./config");
+const { getJson } = require("./feed");
+const url = require("url");
+
+//server
+http
+  .createServer(async (req, res) => {
+    let channelID = url.parse(req.url).pathname.split("/")[2];
+    res.writeHead(200, { "content-type": "text/json" });
+    if (req.method === "GET") {
+      await getJson(channelID, res);
+    }
+  })
+  .listen(port, () => console.log(`app running in 8000`));
